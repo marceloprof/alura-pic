989c26a5f16aee65894c87d4601e96d4ef6847cc
marceloprof <eck.marcelo@gmail.com>
2018-07-08 20:05:29 -0300

    vue-js training first commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app" class="corpo">
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <ul class="lista-fotos">
+      <li v-for="foto of fotos" class="lista-fotos-item">
+        <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo"/>
+        </meu-painel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import Painel from './components/shared/painel/Painel.vue'
+  export default {
+    components:{
+      'meu-painel': Painel
+    },
+
+    data () {
+      return {
+        titulo: 'Alurapic',
+        fotos:[]
+      }
+    },
+    created(){
+        this.$http.get('http://localhost:3000/v1/fotos')
+          .then(res => res.json())
+          .then(fotos => this.fotos = fotos);
+    }
+}
+</script>
+
+<style lang="scss">
+  .centralizado {
+    text-align: center;
+  }
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item {
+    display: inline-block;
+  }
+
+  .imagem-responsiva {
+    width: 100%;
+  }
+
+  /* estilo do painel */
+
+  .painel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .painel .painel-titulo {
+    text-align: center;
+    border: solid 2px;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
+</style>
